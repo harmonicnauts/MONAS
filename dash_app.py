df32146164d85effb2fe9073b94a4660dca0d6e5
BuildTools <nasutionrachmat@gmail.com>
2023-12-05 10:21:46 +0700

    terjadi lagi (4x)

--- dash_app.py
+++ dash_app.py
@@ -631,5 +631,5 @@ def upt_click(feature, tabs_value):
                 )
 
 if __name__ == '__main__':
-    # app.run_server(host= '0.0.0.0',debug=False)
-    app.run_server(host= '127.0.0.1',debug=True)
+    app.run_server(host= '0.0.0.0',debug=False)
+    # app.run_server(host= '127.0.0.1',debug=True)
