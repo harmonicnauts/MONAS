2d5a91eea23e545af7dda66f22a40b051a08707c
Nycht1 <nasutionrachmat@gmail.com>
2024-01-11 13:29:49 +0700

    run

--- dash_app.py
+++ dash_app.py
@@ -678,5 +678,5 @@ app.layout = html.Div([
 
 
 if __name__ == '__main__':
-    # app.run_server(host= '0.0.0.0',debug=False)
-    app.run_server(host= '127.0.0.1',debug=True)
+    app.run_server(host= '0.0.0.0',debug=False)
+    # app.run_server(host= '127.0.0.1',debug=True)
